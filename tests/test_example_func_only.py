46cfbaf0c9ef877a30edbb9708746c589770c01d
vlad <vladimir.shkodin@slickdeals.net>
2021-02-16 08:55:15 -0800

    moving tests around

---
+++ tests/test_example_func_only.py
@@ -0,0 +1,5 @@
+def test_junk():
+    assert 1
+
+def test_boom():
+    assert 1
---
+++ tests/test_example_module_with_class.py
@@ -0,0 +1,6 @@
+class TestClass:
+    def test_inclass_one(self):
+        assert 0
+
+    def test_inclass_two(self):
+        assert 0
