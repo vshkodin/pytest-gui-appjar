2e81ced71c29c5f261a513b7d7938b5671f0512b
vsshk <v.s.shkodin@gmail.com>
2021-02-17 23:52:21 -0800

    path config

--- lib/gui/subWindowSettings.py
+++ lib/gui/subWindowSettings.py
@@ -13,43 +13,69 @@ class Settings:
         self.app.addLabel("CurrentAddopts", f" Current addopts : {configPytest.get('pytest','addopts')}", 1, 0)
 
         self.app.addLabel("testPath", "Tests path", 2, 0)
-        self.app.addEntry("testPathField", 2, 1)
-        self.app.setEntryDefault("testPathField", configPytest.get('pytest','testpaths'))
+        #self.app.addEntry("testPathField", 2, 1)
+        self.app.addButtons(["modify Paths",],self.update, 2, 1)
+        #self.app.setEntryDefault("testPathField", configPytest.get('pytest','testpaths'))
 
         self.app.addLabel("addoptsLabel", "Addopts", 3, 0)
-        self.app.addEntry("addoptsField", 3, 1)
-        self.app.setEntryDefault("addoptsField", configPytest.get('pytest','addopts'))
+        self.app.addButtons(["modify Addops", ], self.update, 3, 1)
+        #self.app.addEntry("addoptsField", 3, 1)
+        #self.app.setEntryDefault("addoptsField", configPytest.get('pytest','addopts'))
 
         #self.app.addLabel("l2", "Password", 1, 0)
         #self.app.addEntry("Password", 1, 1)
-        self.app.addButtons(["Update", "Close"], self.press, 4, 0, 2)
+        self.app.addButtons(["Close"], self.press, 4, 0, 2)
         #self.app.stopLabelFrame()
         # app.addNamedCheckBox(i, i)
         self.app.setSize(400, 400)
         self.app.stopSubWindow()
 
-    def press(self, button):
-        if button == "Update":
-            if self.app.yesNoBox("YES&NO", "Are you Sure") == True:
-                oldPath = configPytest.get('pytest', 'testpaths')
-                new= self.app.getEntry("testPathField")
-                oldAddopts = configPytest.get('pytest', 'addopts')
-                newAddopts = self.app.getEntry("addoptsField")
-                if oldPath != new:
-                    configPytest.set('pytest', 'testpaths', new)
-                    with open('pytest.ini', 'w') as f:
-                        configPytest.write(f)
-                    self.app.setLabel("CurrentTestPath", f" Current testPath : {configPytest.get('pytest','testpaths')}")
-                    self.app.setEntryDefault("testPathField",  configPytest.get('pytest','testpaths'))
-                    #self.app.hideSubWindow("Settings")
-                    #self.app.showSubWindow("Settings")
-                if oldAddopts != newAddopts:
-                    configPytest.set('pytest', 'addopts', newAddopts)
-                    with open('pytest.ini', 'w') as f:
-                        configPytest.write(f)
-                    self.app.setLabel("CurrentAddopts", f" Current addopts : {configPytest.get('pytest', 'addopts')}")
-                    self.app.setEntryDefault("addoptsField", configPytest.get('pytest', 'addopts'))
+    def update(self,button):
+        if button== "modify Paths":
+            reply=self.app.textBox("Update value", f"Update testpath")
+            if reply is not None:
+                configPytest.set('pytest', 'testpaths', reply)
+                with open('pytest.ini', 'w') as f:
+                    configPytest.write(f)
+                self.app.setLabel("CurrentTestPath", f" Current testPath : {configPytest.get('pytest', 'testpaths')}")
+                self.app.showSubWindow("Settings")
             else:
-                pass
-        elif button == "Close":
+                self.app.showSubWindow("Settings")
+        elif button== "modify Addops":
+            reply=self.app.textBox("Update value", f"Update Addopts")
+            if reply is not None:
+                configPytest.set('pytest', 'addopts', reply)
+                with open('pytest.ini', 'w') as f:
+                    configPytest.write(f)
+                self.app.setLabel("CurrentAddopts", f" Current addopts : {configPytest.get('pytest', 'addopts')}")
+                self.app.showSubWindow("Settings")
+            else:
+                self.app.showSubWindow("Settings")
+
+
+
+    def press(self, button):
+        # if button == "Update":
+        #     if self.app.yesNoBox("YES&NO", "Are you Sure") == True:
+        #         oldPath = configPytest.get('pytest', 'testpaths')
+        #         new= self.app.getEntry("testPathField")
+        #         oldAddopts = configPytest.get('pytest', 'addopts')
+        #         newAddopts = self.app.getEntry("addoptsField")
+        #         if oldPath != new:
+        #             configPytest.set('pytest', 'testpaths', new)
+        #             with open('pytest.ini', 'w') as f:
+        #                 configPytest.write(f)
+        #             self.app.setLabel("CurrentTestPath", f" Current testPath : {configPytest.get('pytest','testpaths')}")
+        #             self.app.setEntryDefault("testPathField",  configPytest.get('pytest','testpaths'))
+        #             #self.app.hideSubWindow("Settings")
+        #             #self.app.showSubWindow("Settings")
+        #         if oldAddopts != newAddopts:
+        #             configPytest.set('pytest', 'addopts', newAddopts)
+        #             with open('pytest.ini', 'w') as f:
+        #                 configPytest.write(f)
+        #             self.app.setLabel("CurrentAddopts", f" Current addopts : {configPytest.get('pytest', 'addopts')}")
+        #             self.app.setEntryDefault("addoptsField", configPytest.get('pytest', 'addopts'))
+        #     else:
+        #         pass
+        if button == "Close":
             self.app.hideSubWindow("Settings")
