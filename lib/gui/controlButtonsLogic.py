db4258651f9d9cb1aaf68884d56376172ca491b9
vsshk <v.s.shkodin@gmail.com>
2021-02-20 12:54:52 -0800

    test results view in test runner

--- lib/gui/controlButtonsLogic.py
+++ lib/gui/controlButtonsLogic.py
@@ -1,7 +1,6 @@
-import os,subprocess
+import subprocess
 from lib.tools.log import log
 from lib.tools.cleanReports import clean_report
-from lib.tools.resultsGetter import get_results
 from lib.tools.testRunner import test_runner
 from .scrollPaneView import ScrollPaneView
 
@@ -36,9 +35,7 @@ class ControlButtonsLogic:
 
         elif self.button == "Run selected":
             l = self.app.getAllCheckBoxes()
-            test_runner([i for i in l if l[i]==True])
-            self.app.setLabel("test_results", get_results())
+            test_runner([i for i in l if l[i]==True], app=self.app)
 
         elif self.button == "Run All":
-            test_runner()
-            self.app.setLabel("test_results", get_results())
+            test_runner(app=self.app)
--- lib/tools/testRunner.py
+++ lib/tools/testRunner.py
@@ -1,8 +1,10 @@
 import pytest
 from lib.tools.log import log
 from lib.tools.cleanReports import clean_report
+from lib.tools.resultsGetter import get_results
 
-def test_runner(*args):
+
+def test_runner(*args, app):
     clean_report()
     log.info(f'test run {args}')
     if len(args)==0:
@@ -12,3 +14,4 @@ def test_runner(*args):
     else:
         l=['--alluredir=reports']+args[0]
         pytest.main(l)
+    app.setLabel("test_results", get_results())
